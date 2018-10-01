4c033b44d895224127832e80ead9e20bc83e4221
sachinsaxena021988 <43206328+sachinsaxena021988@users.noreply.github.com>
2018-10-01 14:06:05 +0530

    vowel check

---
+++ 2.2 -vowelCheck.py
@@ -0,0 +1,6 @@
+def checkvowel(char):
+   return(char=='a' or char=='A' or char=='e' or char=='E' or char=='i' or char=='I'
+       or char=='o' or char=='O' or char=='u' or char=='U')
+       
+       
+checkvowel('b')
